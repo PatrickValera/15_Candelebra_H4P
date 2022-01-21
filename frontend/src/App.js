9009e41adf3f96ddc98967fc8bdb36f7a0c9bb07
saish0 <patrickvalera50@yahoo.com>
2022-01-21 15:44:28 -0800

    first commit | boiler plate F.E.

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { theme } from './themes/theme'
+import { ThemeProvider } from '@mui/material/styles';
+import { Button, CssBaseline, Paper } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './screens/Home';
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* <Header /> */}
+      <Navigation />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path='/dashboard' element={</>}/> */}
+        </Routes>
+      </Router>
+
+    </ThemeProvider>
+  );
+}
+
+export default App;
