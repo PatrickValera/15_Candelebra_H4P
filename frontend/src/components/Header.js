d167caf3b32a6a75e430545ac26b635ecab86cb0
saish0 <patrickvalera50@yahoo.com>
2022-01-23 08:47:27 -0800

    images

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,8 +3,11 @@ import React from 'react';
 
 const Header = () => {
   return (
-      <Box>
-          <Typography varaint='h1'>Candelebra</Typography>
+      <Box display='flex' sx={{alignItems:'center'}}>
+          <Box sx={{width:'35px'}}>
+              <img className='image-fit-cover' src='./candle.png' alt='logo?'/>
+          </Box>
+          <Typography varaint='h2'>CANDELEBRA</Typography>
       </Box>
   )
 };
