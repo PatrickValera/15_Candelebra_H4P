ceadf0a7e10fa00931dbe0eced06fc56e33488b0
saish0 <patrickvalera50@yahoo.com>
2022-01-23 01:11:41 -0800

    side info panel

--- frontend/src/components/PortfolioPanel.js
+++ frontend/src/components/PortfolioPanel.js
@@ -1,38 +1,100 @@
-import { Box, Typography } from '@mui/material';
-import React from 'react';
+import { Box, Divider, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { toLocale } from '../utils';
+import { RiMoneyDollarBoxFill, RiBarChartBoxFill } from "react-icons/ri";
 
-const PortfolioPanel = ({cash,portfolio}) => {
-    return (
-        <Box sx={{p:2}}>
-            <Typography variant='h4'>My portfolio</Typography>
-            <Box display='flex'>
-                <Box sx={{ flex: '0 0 50px' }}>
-                    C
+const calcGain = (portfolio) => {
+    let gain = 0
+    for (const property in portfolio) {
+        gain += portfolio[property].value - portfolio[property].shares * portfolio[property].avgCost
+    }
+    return gain
+}
+const calcPercent = (portfolio) => {
+    let totalCost = 0
+    let totalVal = 0
+    for (const prop in portfolio) {
+        let val=portfolio[prop].value
+        let purchaseCost=portfolio[prop].shares*portfolio[prop].avgCost
+        totalCost+=purchaseCost
+        totalVal+=val
+    }
+    return((totalVal-totalCost)/totalCost)
+}
+    const PortfolioPanel = ({ cash, portfolio, total: totalInvestment }) => {
+        const [gain, setGain] = useState(0)
+        const [gainPercent, setGainPercent] = useState(0)
+
+        useEffect(() => {
+            console.log(portfolio)
+            if (portfolio) {
+                setGain(calcGain(portfolio))
+                setGainPercent(calcPercent(portfolio))
+            }
+
+        }, [portfolio])
+
+        return (
+            <Box sx={{ p: 2, pt: 4, position: 'sticky', top: '50px' }}>
+                <Typography variant='h3' fontWeight={600} sx={{ mb: '20px' }}>My Portfolio</Typography>
+
+                {/* ====== CASH CONTAINER =================*/}
+                <Box display='flex' sx={{ alignItems: 'center', my: 1 }}>
+                    <Box sx={{ flex: '0 0 40px' }}>
+                        <RiMoneyDollarBoxFill size='100%' color='secondary.main' />
+                    </Box>
+                    <Box sx={{ px: 2, flex: '1 1 auto' }}>
+                        <Typography variant='body2'>
+                            Total Portfolio Value
+                        </Typography>
+                        <Typography variant='h5' fontWeight={600} >
+                            $ {toLocale(Number(totalInvestment) + cash)}
+                        </Typography>
+                    </Box>
                 </Box>
-                <Box sx={{ flex: '1 1 auto' }}>
-                    <Typography variant='body2'>
-                        Cash
-                    </Typography>
-                    <Typography variant='body2'>
-                        + $ {cash}
-                    </Typography>
+                <Divider />
+                <Box display='flex' sx={{ alignItems: 'center', my: 1 }}>
+                    <Box sx={{ flex: '0 0 40px' }}>
+                        <RiMoneyDollarBoxFill size='100%' color='secondary.main' />
+                    </Box>
+                    <Box sx={{ px: 2, flex: '1 1 auto' }}>
+                        <Typography variant='body2'>
+                            Cash
+                        </Typography>
+                        <Typography variant='h6' fontWeight={600} >
+                            $ {toLocale(cash)}
+                        </Typography>
+                    </Box>
+                </Box>
+                {/* ====== END CASH CONTAINER ==============*/}
+
+                {/* ====== GAIN CONTAINER ==================*/}
+                <Box display='flex'>
+                    <Box sx={{ flex: '0 0 40px' }}>
+                        <RiBarChartBoxFill size='100%' />
+                    </Box>
+                    <Box sx={{ px: 2, flex: '1 1 auto' }}>
+                        <Typography variant='body2' >
+                            Investments
+                        </Typography>
+                        <Typography variant='h6' fontWeight={600} >
+                            $ {toLocale(totalInvestment)}
+                        </Typography>
+                        <Typography variant='body1' fontWeight={600} color={gain > 0 ? 'success.main' : 'error.main'}>
+                            % {toLocale((gainPercent * 100))}
+                        </Typography>
+                    </Box>
+                </Box>
+                {/* ====== END GAIN CONTAINER ==================*/}
+
+                {/* ====== POSITIONS CONTAINER ==================*/}
+                <Box>
+                    <Typography variant='body2'>FP: {portfolio['FP'] && toLocale(portfolio['FP'].value)}</Typography>
+                    <Typography variant='body2'>XIN: {portfolio['XIN'] && toLocale(portfolio['XIN'].value)}</Typography>
+                    <Typography variant='body2'>PAL: {portfolio['PAL'] && toLocale(portfolio['PAL'].value)}</Typography>
                 </Box>
             </Box>
-            <Box display='flex'>
-                <Box sx={{ flex: '0 0 50px' }}>
-                    IC
-                </Box>
-                <Box sx={{ flex: '1 1 auto' }}>
-                    <Typography variant='body2'>
-                        Gain
-                    </Typography>
-                    <Typography variant='body2'>
-                        + $ 3,123
-                    </Typography>
-                </Box>
-            </Box>
-        </Box>
-    )
-};
+        )
+    };
 
-export default PortfolioPanel;
+    export default PortfolioPanel;
---
+++ frontend/src/screens/Home.tsx
@@ -0,0 +1,109 @@
+import { Box, Typography } from '@mui/material';
+import React, { Profiler, useEffect, useState } from 'react';
+import Chart from '../components/Chart'
+import PortfolioPanel from '../components/PortfolioPanel'
+
+
+const tickers = [{
+  ticker: 'PAN',
+  name: 'Frying Pan',
+  color: '#EFEF51',
+  startingValue: 200
+}, {
+  ticker: 'XIN',
+  name: 'Xin inc.',
+  color: '#EFA051',
+  startingValue: 150
+}, {
+  ticker: 'SIMP',
+  name: 'Flepal LLC.',
+  color: '#51A0EF',
+  startingValue: 100
+}, {
+  ticker: 'ZAO',
+  name: 'Irene Zhao',
+  color: '#EF51A0',
+  startingValue: 15000
+}, {
+  ticker: 'CYNO',
+  name: 'CYNO Limited',
+  color: '#C548C5',
+  startingValue: 10000
+}, {
+  ticker: 'AWAD',
+  name: 'AWAD corp.',
+  color: '#48C5C5',
+  startingValue: 700
+},]
+const Home = () => {
+  const [total, setTotal] = useState(0)
+  const [cash, setCash] = useState(10000)
+  const [portfolio, setPortfolio] = useState({})
+
+  // const calcTotal = () => {
+  //   let tot=0
+  //   console.log('CALCULATING=====')
+  //   for (const prop in portfolio) {
+  //     console.log(prop)
+  //     let aNum=portfolio[prop].value
+  //     if(portfolio[prop])tot = tot+aNum
+  //     console.log(tot)
+
+  //   }
+  //   setTotal(tot)
+  // }
+  // useEffect(() => {
+  //   // console.log('hello')
+  //   // console.log(portfolio)
+  //   const totalInterval = setInterval(() => {
+  //     console.log('starting interval')
+  //     calcTotal()
+  //   },5000)
+  //   return () => {
+  //     console.log('cleaning up intervals')
+  //     clearInterval(totalInterval);
+  // }
+
+
+  // }, [])
+  useEffect(()=>{
+      let tot=0
+    console.log('CALCULATING=====')
+    for (const prop in portfolio) {
+      console.log(prop)
+      let aNum=portfolio[prop].value
+      if(portfolio[prop])tot = tot+aNum
+      console.log(tot)
+
+    }
+    setTotal(tot)
+  },[portfolio])
+
+  return (
+    <>
+      <Box display='flex' sx={{position:'relative'}}>
+        {/* ========HEADER============================ */}
+        <Box sx={{ flex: '1 1 200px', p: 3 }}>
+          <Box>
+            <Typography variant='h1' fontWeight='700'>Stonk Market</Typography>
+            <Typography variant='h5' color='grey.600'>Trending Stonks</Typography>
+
+          </Box>
+          {/* ========TICKERS============================ */}
+          <Box display='flex' sx={{ width: '100%', flexWrap: 'wrap', p: 1 }}>
+            {tickers.map((tick, index) => (
+              <Chart key={index} tick={tick} setPortfolio={setPortfolio} setCash={setCash} portfolio={portfolio} cash={cash} />
+            ))}
+          </Box>
+        </Box>
+        {/* ========SIDE============================ */}
+        <Box sx={{ flex: '0 0 300px' }}>
+          <PortfolioPanel cash={cash} portfolio={portfolio} total={total} />
+        </Box>
+
+      </Box>
+    </>
+  )
+};
+
+export default Home;
